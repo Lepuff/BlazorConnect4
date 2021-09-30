275a14f3cdb962b6a383eeb4a2b4552a69126b57
Lepuff <oskar11.persson@gmail.com>
2021-09-30 15:48:48 +0200

    Added timers to the training function to know what takes time

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 namespace BlazorConnect4
 {
@@ -37,21 +38,29 @@ namespace BlazorConnect4
 
         public static void Training()
             {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine("started loading AI's");
+
             
+            AIModels.QAgent RedAi = (AIModels.QAgent)AIModels.AI.FromFile("Data/RedV4.bin");
+            AIModels.QAgent YellowAi = (AIModels.QAgent)AIModels.AI.FromFile("Data/YellowV4.bin");
 
-
-            AIModels.QAgent RedAi = (AIModels.QAgent)AIModels.AI.FromFile("Data/RedV3.bin");
-            AIModels.QAgent YellowAi = (AIModels.QAgent)AIModels.AI.FromFile("Data/YellowV3.bin");
-
+            TimeSpan ts = sw.Elapsed;
+            Console.WriteLine("Done loading AI's");
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
             //AIModels.QAgent RedAi = new AIModels.QAgent(Model.CellColor.Red);
             //AIModels.QAgent YellowAi = new AIModels.QAgent(Model.CellColor.Yellow);
 
-            AIModels.RandomAI randomAI = new AIModels.RandomAI();
+            //AIModels.RandomAI randomAI = new AIModels.RandomAI();
             //RedAi.WorkoutV2( randomAI, 1000);
             //YellowAi.WorkoutV2(randomAI, 1000);
 
 
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 Console.WriteLine(i);
                 if (i % 2 == 0)
@@ -63,12 +72,27 @@ namespace BlazorConnect4
                     RedAi.WorkoutV2(YellowAi, 1000);
                 }
             }
+            ts = sw.Elapsed;
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+            Console.WriteLine("saving AI's");
+            RedAi.ToFile("Data/RedV4.bin");
+            YellowAi.ToFile("Data/YellowV4.bin");
+            
 
-            RedAi.ToFile("Data/RedV3.bin");
-            YellowAi.ToFile("Data/YellowV3.bin");
 
 
 
+            ts = sw.Elapsed; ts = sw.Elapsed;
+            sw.Stop();
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+            
+            Console.WriteLine("Done Saving");
             Console.WriteLine();
         }
     }
